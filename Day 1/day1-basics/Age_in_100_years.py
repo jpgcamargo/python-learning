0c655e7a972d215dfafde0d5d257d0b854b65750
Joao Pedro G. de Camargo <jcamargo@starrett.com>
2025-08-08 14:29:14 -0400

    Day 1:variables, data types, printing

---
+++ Day 1/day1-basics/Age_in_100_years.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+year_birth = 2025-age
+year_100 = year_birth + 100
+
+print(f"Hello, {name}! You will turn 100 years old in the year {year_100}.")
---
+++ Day 1/day1-basics/variables.py
@@ -0,0 +1,12 @@
+##EXERCISE 1
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+color = input("Enter your favorite color: ")
+
+print("Hello, " + name + "! You are " + age + " years old and your favorite color is " + color + ".")
+
+## EXERCISE 2
+num1 = float(input("Enter the first number: "))
+num2=  float(input("Enter the second number: "))
+sum_result = num1 + num2
+print(f"The sum of {num1} and {num2} is {sum_result} ")
