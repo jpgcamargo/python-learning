f129ea824f21708b15832a64bbc218f999438a7b
Joao Pedro G. de Camargo <jcamargo@starrett.com>
2025-08-08 14:57:36 -0400

    Day 2: strings and basic math operations

---
+++ day2-basics/math_.py
@@ -0,0 +1,12 @@
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+print(f"Sum: {(a+b):.2f}")
+print(f"Difference: {(a-b):.2f}")
+print(f"Product:{(a*b):.1f}")
+print(f"Division: {(a/b):.3f}")
+print(f"Remainder: {(a%b):.4f}")
+print(f"Power:{(a**b):.5f}")
+
+
+
---
+++ day2-basics/strings.py
@@ -0,0 +1,4 @@
+sentence = input("Enter a sentence: ")
+print(sentence.upper())
+print(sentence.lower())
+print(f"Length: {len(sentence)} characters")
